8d17668e0a4bf8260937febd0dda9d1e7ef26ed0
Andrew Aquino <dawneraq@gmail.com>
2023-11-09 12:35:55 -0800

    style Swatch w/emotion

--- src/components/Swatch.tsx
+++ src/components/Swatch.tsx
@@ -1,4 +1,5 @@
-import './Swatch.css';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
 interface SwatchProps {
 	color: string;
@@ -7,10 +8,22 @@ interface SwatchProps {
 export default function Swatch({ color }: SwatchProps) {
 	return (
 		<figure
-			className="Swatch"
 			style={
 				{ '--color-swatch': `var(--color-${color})` } as React.CSSProperties
 			}
+			css={css({
+				display: 'flex',
+				gap: '1rem',
+				alignItems: 'center',
+
+				'&::before': {
+					content: '""',
+					width: '2rem',
+					aspectRatio: 1,
+					outline: '1px solid black',
+					backgroundColor: 'var(--color-swatch)',
+				},
+			})}
 		>
 			<figcaption>{color.replace('-', ' ')}</figcaption>
 		</figure>
