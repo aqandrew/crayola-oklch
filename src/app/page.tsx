7b5b45e84bb7af95de7fc13452d8d640b592b7f5
Andrew Aquino <dawneraq@gmail.com>
2023-11-09 11:56:06 -0800

    use emotion to define color CSS vars from JS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,32 @@
+'use client';
+
+import { Global } from '@emotion/react';
+
+import { COLORS } from '@/utils/colors';
 import Swatch from '@/components/Swatch';
 
 export default function Home() {
 	return (
-		<main>
-			<h1>hello</h1>
+		<>
+			<Global
+				styles={{
+					':root': Object.entries(COLORS).reduce(
+						(styles, [colorName, color]) => ({
+							...styles,
+							['--color-' + colorName]: color,
+						}),
+						{}
+					),
+				}}
+			/>
 
-			{COLORS.map((color) => (
-				<Swatch color={color} key={color} />
-			))}
-		</main>
+			<main>
+				<h1>hello</h1>
+
+				{Object.keys(COLORS).map((color) => (
+					<Swatch color={color} key={color} />
+				))}
+			</main>
+		</>
 	);
 }
-
-// TODO switch to CSS-in-JS so that there can be just one source of truth
-const COLORS = [
-	'red',
-	'scarlet',
-	'red-orange',
-	'orange',
-	'yellow-orange',
-	'yellow',
-	'green-yellow',
-	'yellow-green',
-	'green',
-	'blue-green',
-	'cerulean',
-	'blue',
-	'bluetiful',
-	'indigo',
-	'blue-violet',
-	'violet',
-	'red-violet',
-	'violet-red',
-	'bittersweet',
-	'banana-mania',
-	'granny-smith',
-	'jungle-green',
-	'robins-egg',
-	'royal-purple',
-	'plum',
-	'wild-strawberry',
-];
---
+++ src/utils/colors.ts
@@ -0,0 +1,35 @@
+// hex codes sampled from here with Digital Color Meter
+// https://www.instagram.com/reel/CxviX3QrBgY/?hl=en
+// oklch values obtained from https://oklch.com/
+
+export const COLORS = {
+	// original 24-pack
+	red: 'oklch(58.43% 0.232 25.43)', // #e51027
+	scarlet: 'oklch(63.84% 0.234 26.09)', // #fa3035
+	'red-orange': 'oklch(59.85% 0.221 27.53)', // #e62b29
+	orange: 'oklch(70.72% 0.178 47.71)', // #f67727
+	'yellow-orange': 'oklch(71.07% 0.161 56.38)', // #eb8325
+	yellow: 'oklch(79% 0.159 92.08)', // #dfb612
+	'green-yellow': 'oklch(79.07% 0.156 97.19)', // #d7ba20
+	'yellow-green': 'oklch(70.2% 0.152 123.8)', // #8cae33
+	green: 'oklch(53.28% 0.137 147.52)', // #24813a
+	'blue-green': 'oklch(52.23% 0.12382028137144023 241.9488014959934)', // #0070aa
+	cerulean: 'oklch(50.49% 0.16262266110737159 254.35290114509638)', // #0063be
+	blue: 'oklch(45.71% 0.166 257.59)', // #0052b1
+	bluetiful: 'oklch(33.17% 0.11694152904668356 256.8281093396693)', // #003370
+	indigo: 'oklch(35.22% 0.145 261.46)', // #063385
+	'blue-violet': 'oklch(29.43% 0.124 266.93)', // #112369
+	violet: 'oklch(40.26% 0.124 314.09)', // #602f76
+	'red-violet': 'oklch(44.59% 0.179 4.32)', // #9c0048
+	'violet-red': 'oklch(61.7% 0.237 16.95)', // #f21b4f
+
+	// additions for proposed 24-pack
+	bittersweet: 'oklch(65.76% 0.217 29.1)', // #fa4839
+	'banana-mania': 'oklch(76.68% 0.153 75.07)', // #eba324
+	'granny-smith': 'oklch(69.04% 0.091 141.75)', // #7baa75
+	'jungle-green': 'oklch(55.4% 0.093 187.2)', // #11847c
+	'robins-egg': 'oklch(54.64% 0.10062293895614662 221.56836171458156)', // #007d9a
+	'royal-purple': 'oklch(35.44% 0.121 298.29)', // #452871
+	plum: 'oklch(38.17% 0.13 330.74)', // #672162
+	'wild-strawberry': 'oklch(58.82% 0.23674007941731795 21.429836265688046)', // #e80037
+};
